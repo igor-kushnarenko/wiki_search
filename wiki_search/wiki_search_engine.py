07f9447fea0165e7023c377a5ee700cebc7229b1
igors <anapa.igor@yandex.ru>
2021-08-19 11:47:34 +0300

    -добавлена возможность с выбором дополнительных вариантов поиска;

--- wiki_search/wiki_search_engine.py
+++ wiki_search/wiki_search_engine.py
@@ -6,22 +6,26 @@ import json
 
 
 class DataBase:
+    """Класс содержащий методы для работы с Базой Данных"""
     def __init__(self, db_name):
         self.db = db_name
         if not self.db:
             self.create_new_db()
 
     def create_new_db(self):
+        """Создание новой БД"""
         data = {}
         with open(self.db, 'w') as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
 
     def read_db(self):
+        """Чтение БД"""
         with open(self.db, 'r') as f:
             data = json.load(f)
         return data
 
     def write_db(self, page_list, data, response):
+        """Запись в БД спарсенных данных из wikipedia"""
         page = page_list[0]
         choose = page_list[1]
         title = page.title
@@ -41,16 +45,19 @@ class DataBase:
 
 
 class WikiSearchEngine:
+    """Поиск по БД и в wikipedia"""
     def __init__(self, response, data):
         self.response = response
         self.data = data
 
     def search_to_db(self):
+        """Поиск в БД по ключевому слову"""
         for key, value in self.data.items():
             if self.response == key:
                 return value
 
     def search_to_web(self):
+        """Поиск в wikipedia с обработкой исключений связанных с опечатками при вводе"""
         self.response = self.response.lower().capitalize()
         try:
             page = wikipedia.page(self.response)
@@ -65,6 +72,7 @@ class WikiSearchEngine:
 
 
 class TerminalMenu:
+    """Класс принимающий ключевое слово и выводящий меню для пользователя"""
     def take_result(self, response):
         db = DataBase('wiki_data.json')
         data = db.read_db()
@@ -75,30 +83,49 @@ class TerminalMenu:
         else:
             page = search.search_to_web()
             result = db.write_db(page, data, response)
-            search = page[1]  # TODO добавить возможные варианты
             return result
 
-    def main_menu(self):
+    def main_menu(self, response):
         wikipedia.set_lang('ru')
         while True:
-            response = input('Введите запрос: ')
             result = self.take_result(response)
-            print(f'{result["summary"]}\n{result["url"]}')
-            print('1.ПОДБРОБНЕЕ\n2.НОВЫЙ ПОИСК\n3.ВЫХОД')
+            print(f'\n{result["summary"]}\n{result["url"]}')
+            print('\n1.ПОДБРОБНЕЕ\n2.НОВЫЙ ПОИСК\n3.ВЫХОД')
             chooses = [f'{index+1}. {x}' for index, x in enumerate(result['choose'], 3)]
             print(f'! ВАМ МОЖЕТ БЫТЬ ИНТЕРЕСНО: {", ".join(chooses)}')
-
             user_input = int(input('Ввод: '))
             if user_input == 1:
-                print(result['content'])
+                print(f"\n{result['content']}")
             elif user_input == 2:
-                continue
+                self.run()
             elif user_input == 3:
                 break
             elif user_input == 4:
-                self.take_result(chooses[0])
+                TerminalMenu().main_menu(chooses[0])
+            elif user_input == 5:
+                TerminalMenu().main_menu(chooses[1])
+            elif user_input == 6:
+                TerminalMenu().main_menu(chooses[2])
+            elif user_input == 7:
+                TerminalMenu().main_menu(chooses[3])
+            elif user_input == 8:
+                TerminalMenu().main_menu(chooses[4])
+            elif user_input == 9:
+                TerminalMenu().main_menu(chooses[5])
+            elif user_input == 10:
+                TerminalMenu().main_menu(chooses[6])
+            elif user_input == 11:
+                TerminalMenu().main_menu(chooses[7])
+            elif user_input == 12:
+                TerminalMenu().main_menu(chooses[8])
+            elif user_input == 13:
+                TerminalMenu().main_menu(chooses[9])
+
+    def run(self):
+        response = input('Введите запрос: ')
+        TerminalMenu().main_menu(response)
 
 
 if __name__ == '__main__':
-    TerminalMenu().main_menu()
+    TerminalMenu().run()
     # DataBase('wiki_data.json').create_new_db()
